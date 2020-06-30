4167255a5e91e009ac5d6974774166e0df63fcf2
LeraLam <paul.l.l@hotmail.fr>
2020-06-30 23:35:51 +0200

    esx_gunalert : V2.0

--- client.lua
+++ client.lua
@@ -12,24 +12,27 @@ Citizen.CreateThread(function()
 
     while (Config.EnableGunAlert) do
         local playerPed = PlayerPedId()
-        local pedPosition = GetEntityCoords(playerPed)
-        if (ESX.PlayerData.job.name ~= Config.PoliceJobName) then
-            local willTrigger = (math.random(0, 100) < Config.ProbabilityOfTriggetAlert)
-            if  willTrigger then
-                for city, coordinates in pairs(Config.CityCoordinates) do
-                    local checkX =  ((coordinates.x1 <= pedPosition.x) and (pedPosition.x <= coordinates.x2))
-                    local checkY =  ((coordinates.y2 <= pedPosition.y) and (pedPosition.y <= coordinates.y1))
-                    local checkZ = ((coordinates.z1 <= pedPosition.z) and (pedPosition.z <= coordinates.z2))
-                    local inArea = (checkX and checkY and checkZ)
-                    if inArea and IsPedShooting(playerPed) and not IsPedCurrentWeaponSilenced(playerPed)  then
-                        TriggerServerEvent('esx_addons_gcphone:startCall', Config.PolicePhoneNumber, _U('gunalert') , pedPosition, true, {
-                            pedPosition = pedPosition,
-                            })
-                        TriggerServerEvent('esx_gunalert:logshot', pedPosition,  playerPed)
-                        Citizen.Wait(Config.TimerWaitAfterShoot)
+        while IsPedArmed(playerPed, 7) and GetSelectedPedWeapon(playerPed) ~= Config.HashToCheck do --Check if Player is Armed and have weapon selected
+            local pedPosition = GetEntityCoords(playerPed)
+            if (ESX.PlayerData.job.name ~= Config.PoliceJobName) then --Check if Player is not a Police officer
+                local willTrigger = (math.random(0, 100) < Config.ProbabilityOfTriggetAlert)
+                if  willTrigger then
+                    for city, coordinates in pairs(Config.CityCoordinates) do
+                        local checkX =  ((coordinates.x1 <= pedPosition.x) and (pedPosition.x <= coordinates.x2))
+                        local checkY =  ((coordinates.y2 <= pedPosition.y) and (pedPosition.y <= coordinates.y1))
+                        local checkZ = ((coordinates.z1 <= pedPosition.z) and (pedPosition.z <= coordinates.z2))
+                        local inArea = (checkX and checkY and checkZ)
+                        if inArea and IsPedShooting(playerPed) and not IsPedCurrentWeaponSilenced(playerPed)  then --Check if Player is shooting in some cities
+                            TriggerServerEvent('esx_addons_gcphone:startCall', Config.PolicePhoneNumber, _U('gunalert') , pedPosition, true, {
+                                pedPosition = pedPosition,
+                                })
+                            TriggerServerEvent('esx_gunalert:logshot', pedPosition,  playerPed)
+                            Citizen.Wait(Config.TimerWaitAfterShoot)
+                        end
                     end
                 end
             end
+            Citizen.Wait(Config.TimerCheck)
         end
         Citizen.Wait(Config.TimerWait)
     end
--- config.lua
+++ config.lua
@@ -7,13 +7,14 @@ Config.EnableGunAlert = true --True to enable gun's shot alert
 Config.EnableLog = true --True to enable log on gun's shot alert
 Config.PolicePhoneNumber = '17' --To Change according to your server's police's phone number
 Config.PoliceJobName = 'police' --To Change according to your server's police's job name
-Config.TimerWait = 250 --Timer for thread
+Config.TimerWait = 1000 --Timer for thread
 Config.TimerWaitAfterShoot = 120000 --Timer between two alerts coming from the same player
+Config.TimerCheck = 0 --Timer for checking if is ped shooting
 Config.ProbabilityOfTriggetAlert = 100 -- Percent
-
+Config.HashToCheck = GetHashKey('WEAPON_UNARMED') --Hash of the fist
 
 Config.CityCoordinates = {
-    ["southCity"] = {x1 = -2322, y1 = 730, z1 = -200, x2 = 2304, y2 = -3651, z2 = 600},
-    ["northCity"] = {x1 = -1080, y1 = 7186, z1 = -200, x2 = 264, y2 = 5482, z2 = 600},
-    ["trevorCity"] = {x1 = 242, y1 = 3895, z1 = -200, x2 = 2225, y2 = 3516, z2 = 600},
+    ['southCity'] = {x1 = -2322, y1 = 730, z1 = -200, x2 = 2304, y2 = -3651, z2 = 600},
+    ['northCity'] = {x1 = -1080, y1 = 7186, z1 = -200, x2 = 264, y2 = 5482, z2 = 600},
+    ['trevorCity'] = {x1 = 242, y1 = 3895, z1 = -200, x2 = 2225, y2 = 3516, z2 = 600},
 }
